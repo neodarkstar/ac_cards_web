2de102e3f75fca38b38c52daefecb7c49516d71a
John Llombart <johnllombart@gmail.com>
2015-10-17 00:11:25 -0400

    Added missing and duplicate cards to collection section

--- app/scripts/controllers/collection.js
+++ app/scripts/controllers/collection.js
@@ -7,5 +7,24 @@ angular.module('webApp')
       Card.updateCount({ cardId: cardNumber, qty: quantity});
     }
 
+    $scope.counts = {
+      duplicates: 0,
+      total: 0,
+      missing: 0
+    }
+
+    $scope.cards.$promise.then(function(cards){
+      cards.forEach(function(card){
+        if(card.qty > 0){
+          $scope.counts.duplicates += (card.qty - 1);
+          $scope.counts.total += card.qty;
+        }
+
+        if(card.qty == 0){
+          $scope.counts.missing ++;
+        }
+      })
+    })
+
 
   }]);
